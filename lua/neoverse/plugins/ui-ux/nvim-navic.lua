29c21cd1e90b12fe11d90676f727513635326334
Radvil <radvil@@access-mobile.com>
2023-09-04 13:53:34 +0800

    feat: Add ui-ux plugins
    + dressing-nvim
    + noice-nvim
    + nvim-navic
    + nvim-notify

---
+++ lua/neoverse/plugins/ui-ux/nvim-navic.lua
@@ -0,0 +1,59 @@
+-- TODO: set this to universal options
+local kind_icons = {
+  Array = " ",
+  Boolean = " ",
+  Class = " ",
+  Color = " ",
+  Constant = " ",
+  Constructor = " ",
+  Copilot = " ",
+  Enum = " ",
+  EnumMember = " ",
+  Event = " ",
+  Field = " ",
+  File = " ",
+  Folder = " ",
+  Function = " ",
+  Interface = " ",
+  Key = " ",
+  Keyword = " ",
+  Method = " ",
+  Module = " ",
+  Namespace = " ",
+  Null = " ",
+  Number = " ",
+  Object = " ",
+  Operator = " ",
+  Package = " ",
+  Property = " ",
+  Reference = " ",
+  Snippet = " ",
+  String = " ",
+  Struct = " ",
+  Text = " ",
+  TypeParameter = " ",
+  Unit = " ",
+  Value = " ",
+  Variable = " ",
+}
+
+return {
+  "SmiteshP/nvim-navic",
+  lazy = true,
+  init = function()
+    vim.g.navic_silence = true
+    require("minimal.util").on_lsp_attach(function(client, buffer)
+      if client.server_capabilities.documentSymbolProvider then
+        require("nvim-navic").attach(client, buffer)
+      end
+    end)
+  end,
+  opts = function()
+    return {
+      icons = kind_icons,
+      separator = " » ",
+      highlight = true,
+      depth_limit = 5,
+    }
+  end,
+}
