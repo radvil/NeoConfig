886b3042dd3b4e555692a1c14eac24b1d1059f32
Radvil <radvil@@access-mobile.com>
2023-09-04 12:45:13 +0800

    feat: Add nvim-tresitter + context

---
+++ lua/neoverse/plugins/editor/nvim-treesitter.lua
@@ -0,0 +1,65 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  event = { "BufReadPost", "BufNewFile" },
+  build = ":TSUpdate",
+
+  keys = {
+    {
+      "<c-space>",
+      desc = "Treesitter » Init/increase selection",
+      mode = { "n", "x" },
+    },
+    {
+      "<bs>",
+      desc = "Treesitter » Decrease selection",
+      mode = "x",
+    },
+  },
+
+  opts = {
+    ensure_installed = {
+      "bash",
+      "css",
+      "diff",
+      "html",
+      "javascript",
+      "lua",
+      "markdown",
+      "markdown_inline",
+      "python",
+      "query",
+      "regex",
+      "toml",
+      "vim",
+      "yaml",
+    },
+    highlight = {
+      enable = true,
+      additional_vim_regex_highlighting = { "markdown" },
+    },
+    indent = { enable = true },
+    incremental_selection = {
+      enable = true,
+      keymaps = {
+        node_incremental = "<c-space>",
+        init_selection = "<c-space>",
+        scope_incremental = "<nop>",
+        node_decremental = "<bs>",
+      },
+    },
+  },
+
+  config = function(_, opts)
+    if type(opts.ensure_installed) == "table" then
+      local added = {}
+      opts.ensure_installed = vim.tbl_filter(function(lang)
+        if added[lang] then
+          return false
+        end
+        added[lang] = true
+        return true
+      end, opts.ensure_installed)
+    end
+    require("nvim-treesitter.configs").setup(opts)
+  end,
+}
