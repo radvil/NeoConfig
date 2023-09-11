025ca0d78b8cdfdc3c607d558b25a5670d9519ed
Radvil <radvil@@access-mobile.com>
2023-09-11 23:55:01 +0800

    enhancement: Neoverse states

--- lua/neoverse/state.lua
+++ lua/neoverse/state.lua
@@ -1,6 +1,24 @@
 ---@class NeoState
-return {
+local M = {}
+
+---@type NeoState
+local defaults = {
   smooth_cursor = true,
   noice = not vim.g.neovide,
   barbecue = true,
 }
+
+---@type NeoState | nil
+local states = nil
+
+setmetatable(M, {
+  __index = function(_, key)
+    if states == nil then
+      return vim.deepcopy(defaults)[key]
+    end
+    ---@cast states NeoState
+    return states[key]
+  end,
+})
+
+return M
