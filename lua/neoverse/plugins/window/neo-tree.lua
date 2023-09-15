602a3b7b5cdcd90f2ef087bd71ad626a2bf28f81
Radvil <radvil@@access-mobile.com>
2023-09-15 14:45:18 +0800

    fix(neotree): properly extends opts

--- lua/neoverse/plugins/window/neo-tree.lua
+++ lua/neoverse/plugins/window/neo-tree.lua
@@ -51,124 +51,129 @@ return {
     local i = function(icon)
       return string.format("%s ", icon)
     end
-    opts.default_component_configs = {
-      indent = {
-        with_markers = true,
-        with_expanders = false,
-        -- indent_marker = "┊",
-        expander_collapsed = Icons.Folds.Collapsed,
-        expander_expanded = Icons.Folds.Expanded,
-      },
-      git_status = {
-        symbols = {
-          -- Change type
-          added = i(Icons.Git.Added),
-          deleted = i(Icons.Git.Deleted),
-          modified = i(Icons.Git.Modified),
-          renamed = i(Icons.Git.Renamed),
-          -- Status type
-          staged = i(Icons.Git.Staged),
-          unstaged = i(Icons.Git.Unstaged),
-          untracked = i(Icons.Git.Untracked),
-          conflict = i(Icons.Git.Conflict),
-          ignored = i(Icons.Git.Ignored),
+    local Defaults = {
+      default_component_configs = {
+        indent = {
+          with_markers = true,
+          with_expanders = false,
+          -- indent_marker = "┊",
+          expander_collapsed = Icons.Folds.Collapsed,
+          expander_expanded = Icons.Folds.Expanded,
+        },
+        git_status = {
+          symbols = {
+            -- Change type
+            added = i(Icons.Git.Added),
+            deleted = i(Icons.Git.Deleted),
+            modified = i(Icons.Git.Modified),
+            renamed = i(Icons.Git.Renamed),
+            -- Status type
+            staged = i(Icons.Git.Staged),
+            unstaged = i(Icons.Git.Unstaged),
+            untracked = i(Icons.Git.Untracked),
+            conflict = i(Icons.Git.Conflict),
+            ignored = i(Icons.Git.Ignored),
+          },
         },
       },
-    }
-    opts.window = {
-      width = 40,
-      position = "left",
-      mapping_options = {
-        noremap = true,
-        nowait = true,
-      },
-      mappings = {
-        ["l"] = "open",
-        ["w"] = "open_with_window_picker",
-        ["<cr>"] = "open",
-        ["<2-LeftMouse>"] = "open",
-        ["<c-v>"] = "open_vsplit",
-        ["<c-x>"] = "open_split",
-        ["<c-t>"] = "open_tabnew",
-        ["<a-[>"] = "prev_source",
-        ["<a-]>"] = "next_source",
 
-        ["h"] = "close_node",
-        ["za"] = "toggle_node",
-        ["zc"] = "close_node",
-        ["zM"] = "close_all_nodes",
-        ["zR"] = "expand_all_nodes",
-
-        ["a"] = "add",
-        ["d"] = "delete",
-        ["r"] = "rename",
-        ["<F2>"] = "rename",
-        ["c"] = "copy",
-        ["y"] = "copy_to_clipboard",
-        ["x"] = "cut_to_clipboard",
-        ["p"] = "paste_from_clipboard",
-
-        ["?"] = "show_help",
-        ["K"] = { "toggle_preview", config = { use_float = true } },
-        ["<esc>"] = "revert_preview",
-        ["R"] = "refresh",
-      },
-    }
-
-    opts.filesystem = {
-      bind_to_cwd = false,
-      use_libuv_file_watcher = true,
-      hijack_netrw_behavior = "disabled",
-      follow_current_file = {
-        enabled = true,
-        leave_dirs_open = true,
-      },
       window = {
+        width = 44,
+        position = "left",
+        mapping_options = {
+          noremap = true,
+          nowait = true,
+        },
         mappings = {
-          ["."] = "set_root",
-          ["/"] = "fuzzy_finder",
-          ["H"] = "toggle_hidden",
-          ["<bs>"] = "navigate_up",
-          ["f"] = "filter_on_submit",
-          ["[g"] = "prev_git_modified",
-          ["]g"] = "next_git_modified",
-          ["<a-space>"] = "clear_filter",
+          ["l"] = "open",
+          ["w"] = "open_with_window_picker",
+          ["<cr>"] = "open",
+          ["<2-LeftMouse>"] = "open",
+          ["<c-v>"] = "open_vsplit",
+          ["<c-x>"] = "open_split",
+          ["<c-t>"] = "open_tabnew",
+          ["<a-[>"] = "prev_source",
+          ["<a-]>"] = "next_source",
+
+          ["h"] = "close_node",
+          ["za"] = "toggle_node",
+          ["zc"] = "close_node",
+          ["zM"] = "close_all_nodes",
+          ["zR"] = "expand_all_nodes",
+
+          ["a"] = "add",
+          ["d"] = "delete",
+          ["r"] = "rename",
+          ["<F2>"] = "rename",
+          ["c"] = "copy",
+          ["y"] = "copy_to_clipboard",
+          ["x"] = "cut_to_clipboard",
+          ["p"] = "paste_from_clipboard",
+
+          ["?"] = "show_help",
+          ["K"] = { "toggle_preview", config = { use_float = true } },
+          ["<esc>"] = "revert_preview",
+          ["R"] = "refresh",
+        },
+      },
+
+      filesystem = {
+        bind_to_cwd = false,
+        use_libuv_file_watcher = true,
+        hijack_netrw_behavior = "disabled",
+        follow_current_file = {
+          enabled = true,
+          leave_dirs_open = true,
+        },
+        window = {
+          mappings = {
+            ["."] = "set_root",
+            ["/"] = "fuzzy_finder",
+            ["H"] = "toggle_hidden",
+            ["<bs>"] = "navigate_up",
+            ["f"] = "filter_on_submit",
+            ["[g"] = "prev_git_modified",
+            ["]g"] = "next_git_modified",
+            ["<a-space>"] = "clear_filter",
+          },
+        },
+      },
+
+      buffers = {
+        follow_current_file = {
+          enabled = true,
+          leave_dirs_open = true,
+        },
+      },
+
+      source_selector = {
+        winbar = true,
+        statusline = false,
+        truncation_character = "…",
+        show_scrolled_off_parent_node = false, -- HACK: enable this caused flickering on "InsertEnter"
+        highlight_tab = "BufferLineBackground",
+        highlight_separator = "BufferLineBackground",
+        highlight_background = "BufferLineBackground",
+        highlight_tab_active = "BufferLineBufferSelected",
+        highlight_separator_active = "BufferLineIndicatorSelected",
+        sources = {
+          {
+            source = "filesystem",
+            display_name = " 󰙅 files",
+          },
+          {
+            source = "buffers",
+            display_name = "  buffers",
+          },
+          {
+            source = "git_status",
+            display_name = "  git",
+          },
         },
       },
     }
 
-    opts.buffers = {
-      follow_current_file = {
-        enabled = true,
-        leave_dirs_open = true,
-      },
-    }
-
-    opts.source_selector = {
-      winbar = true,
-      statusline = false,
-      truncation_character = "…",
-      show_scrolled_off_parent_node = false, -- HACK: enable this caused flickering on "InsertEnter"
-      highlight_tab = "BufferLineBackground",
-      highlight_separator = "BufferLineBackground",
-      highlight_background = "BufferLineBackground",
-      highlight_tab_active = "BufferLineBufferSelected",
-      highlight_separator_active = "BufferLineIndicatorSelected",
-      sources = {
-        {
-          source = "filesystem",
-          display_name = " 󰙅 files",
-        },
-        {
-          source = "buffers",
-          display_name = "  buffers",
-        },
-        {
-          source = "git_status",
-          display_name = "  git",
-        },
-      },
-    }
+    opts = vim.tbl_deep_extend("force", Defaults, opts or {})
 
     require("neo-tree").setup(opts)
 
