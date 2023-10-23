d9601b8da6868600bf217c0c3f1af0fbdded312a
Radvil <radvil@@access-mobile.com>
2023-10-23 21:38:53 +0800

    feat(lualine): use source from gitsigns for diff sections

--- lua/neoverse/plugins/ui-ux/lualine-nvim.lua
+++ lua/neoverse/plugins/ui-ux/lualine-nvim.lua
@@ -89,6 +89,16 @@ return {
           modified = Config.icons.Git.Modified .. " ",
           removed = Config.icons.Git.Deleted .. " ",
         },
+        source = function()
+          local gitsigns = vim.b.gitsigns_status_dict
+          if gitsigns then
+            return {
+              added = gitsigns.added,
+              modified = gitsigns.changed,
+              removed = gitsigns.removed,
+            }
+          end
+        end,
       },
     }
     local Z = {
