7ecb30f98470a1561c0f3d408332fe5dffb9e386
Radvil <radvil@@access-mobile.com>
2023-10-26 21:18:54 +0800

    fix(window-picker): correctly swap window

--- lua/neoverse/core/specs/nvim-window-picker.lua
+++ lua/neoverse/core/specs/nvim-window-picker.lua
@@ -1,4 +1,27 @@
+local A = vim.api
 local M = {}
+
+local popups = {
+  "TelescopeResults",
+  "TelescopePrompt",
+  "neo-tree-popup",
+  "DressingInput",
+  "flash_prompt",
+  "cmp_menu",
+  "WhichKey",
+  "lspinfo",
+  "notify",
+  "prompt",
+  "mason",
+  "noice",
+  "lazy",
+  "oil",
+}
+
+local sidebars = {
+  "neo-tree",
+  "Outline",
+}
 
 M.opts = {
   show_prompt = false,
@@ -9,63 +32,52 @@ M.opts = {
     include_current = false,
     bo = {
       buftype = { "terminal" },
-      filetype = {
-        "TelescopeResults",
-        "TelescopePrompt",
-        "neo-tree-popup",
-        "DressingInput",
-        "flash_prompt",
-        "cmp_menu",
-        "neo-tree",
-        "WhichKey",
-        "lspinfo",
-        "Outline",
-        "notify",
-        "prompt",
-        "mason",
-        "noice",
-        "lazy",
-        "oil",
-      },
+      filetype = popups,
     },
   },
 }
 
 M.keys = {
+  -- stylua: ignore
   {
     "<a-w>",
     function()
       local win = require("window-picker").pick_window({ include_current_win = false })
-      if not win then
-        win = vim.api.nvim_get_current_win()
-      end
-      vim.api.nvim_set_current_win(win)
+      if not win then return end
+      A.nvim_set_current_win(win)
     end,
-    desc = "Window » Select with picker",
+    desc = "Window » Pick",
   },
   {
     "<leader>ws",
     function()
-      local curr_win = vim.api.nvim_get_current_win()
-      local target_win = require("window-picker").pick_window({ include_current_win = false })
-      if not target_win then
-        target_win = curr_win
-      end
-      local target_ft = vim.api.nvim_get_option_value("filetype", { win = target_win })
-      if vim.tbl_contains(M.opts.filter_rules.bo.filetype, target_ft) then
-        vim.notify("Not possible!", vim.log.levels.WARN, {
-          title = "Window picker",
-        })
+      -- abort if current buffer ft is blacklisted
+      local exclude_fts = vim.list_extend(vim.deepcopy(popups), sidebars)
+      if vim.tbl_contains(exclude_fts, vim.bo.filetype) then
+        require("neoverse.utils").warn("Can't swap current window!", { title = "Window picker" })
         return
       end
-      vim.api.nvim_win_set_buf(target_win, 0)
-      vim.api.nvim_set_current_win(target_win)
-      local target_buf = vim.fn.winbufnr(target_win)
-      if type(target_buf) == "number" then
-        vim.api.nvim_win_set_buf(0, target_buf)
+      local picked_win = require("window-picker").pick_window({
+        include_current_win = false,
+        filter_rules = {
+          bo = {
+            filetype = exclude_fts,
+          },
+        },
+      })
+      if not picked_win then
+        return
       end
+      local source_buf = A.nvim_get_current_buf()
+      local picked_buf = A.nvim_win_get_buf(picked_win)
+      A.nvim_win_set_buf(picked_win, source_buf)
+      vim.schedule(function()
+        A.nvim_win_set_buf(A.nvim_get_current_win(), picked_buf)
+        -- cursor follow new buf's win
+        A.nvim_set_current_win(picked_win)
+      end)
     end,
-    desc = "Swap with picker",
+    desc = "Window » Pick and swap",
   },
 }
 
