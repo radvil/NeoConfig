f5074ecfe382592b67fb56b77d9f86be8c031370
Radvil <radvil@@access-mobile.com>
2024-01-15 02:43:12 +0800

    update typescript language config
    + Add misssing imports

--- lua/neoverse/extras/lang/typescript.lua
+++ lua/neoverse/extras/lang/typescript.lua
@@ -1,4 +1,17 @@
 ---@diagnostic disable: missing-fields
+
+local invoke = function(name)
+  return function()
+    vim.lsp.buf.code_action({
+      apply = true,
+      context = {
+        only = { string.format("source.%s.ts", name) },
+        diagnostics = {},
+      },
+    })
+  end
+end
+
 return {
   {
     "nvim-treesitter/nvim-treesitter",
@@ -23,29 +36,18 @@ return {
           keys = {
             {
               "<leader>co",
-              function()
-                vim.lsp.buf.code_action({
-                  apply = true,
-                  context = {
-                    only = { "source.organizeImports.ts" },
-                    diagnostics = {},
-                  },
-                })
-              end,
-              desc = "Code » Organize Imports",
+              invoke("organizeImports"),
+              desc = "code » organize imports",
             },
             {
               "<leader>cc",
-              function()
-                vim.lsp.buf.code_action({
-                  apply = true,
-                  context = {
-                    only = { "source.removeUnused.ts" },
-                    diagnostics = {},
-                  },
-                })
-              end,
-              desc = "Code » Remove Unused Imports",
+              invoke("removeUnused"),
+              desc = "code » remove unused imports",
+            },
+            {
+              "<leader>cm",
+              invoke("addMissingImports"),
+              desc = "code » add missing imports",
             },
           },
           settings = {
