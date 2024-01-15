d7083e6b08f0f74e63fae3ca5b653c486cb9dee3
Radvil <radvil@@access-mobile.com>
2024-01-16 05:39:12 +0800

    replace persistence with notification after certain actions

--- lua/neoverse/core/specs/persistence-nvim.lua
+++ lua/neoverse/core/specs/persistence-nvim.lua
@@ -1,3 +1,5 @@
+---@diagnostic disable: missing-fields, duplicate-set-field
+
 local M = {}
 
 M.keys = {
@@ -27,5 +29,33 @@ M.keys = {
 M.opts = {
   options = vim.opt.sessionoptions:get(),
 }
+
+M.config = function(_, opts)
+  ---@param msg string
+  ---@param severity string?
+  local notify = function(msg, severity)
+    severity = severity or "info"
+    require("neoverse.utils")[severity](msg, {
+      title = "persistence.nvim",
+      animate = false,
+    })
+  end
+
+  local persistence_nvim = require("persistence")
+  local load_session = persistence_nvim.load
+  local stop_session = persistence_nvim.stop
+
+  persistence_nvim.load = function()
+    load_session()
+    notify("session restored")
+  end
+
+  persistence_nvim.stop = function()
+    stop_session()
+    notify("session stopped", "warn")
+  end
+
+  persistence_nvim.setup(opts)
+end
 
 return M
