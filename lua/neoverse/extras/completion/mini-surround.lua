d82d7a70918ae59b90b9bdd494061760e31c45b7
Radvil <radvil.linux@gmail.com>
2024-03-21 05:37:22 +0800

    fix(mini.surround): Properly register keymap to whichkey

--- lua/neoverse/extras/completion/mini-surround.lua
+++ lua/neoverse/extras/completion/mini-surround.lua
@@ -1,45 +1,47 @@
 return {
-  "echasnovski/mini.surround",
-  keys = function(_, keys)
-    -- Populate the keys based on the user's options
-    local plugin = require("lazy.core.config").spec.plugins["mini.surround"]
-    local opts = require("lazy.core.plugin").values(plugin, "opts", false)
-    local mappings = {
-      { opts.mappings.add, desc = "Surround » Open/add", mode = { "n", "v" } },
-      { opts.mappings.delete, desc = "Surround » Delete" },
-      { opts.mappings.find, desc = "Surround » Find next" },
-      { opts.mappings.find_left, desc = "Surround » Find prev" },
-      { opts.mappings.highlight, desc = "Surround » Highlight" },
-      { opts.mappings.replace, desc = "Surround » Replace/subt" },
-      { opts.mappings.update_n_lines, desc = "Surround » Update 'n' lines config" },
-    }
-    mappings = vim.tbl_filter(function(m)
-      return m[1] and #m[1] > 0
-    end, mappings)
-    return vim.list_extend(mappings, keys)
-  end,
-
-  opts = {
-    silent = true,
-    respect_selection_type = true,
-    mappings = {
-      add = "so",
-      delete = "sd",
-      replace = "sc",
-      find_left = "",
-      find = "",
-      highlight = "sh",
-      update_n_lines = "",
+  {
+    "which-key.nvim",
+    optional = true,
+    opts = {
+      defaults = {
+        ["s"] = { name = "surround" },
+      },
     },
   },
 
-  config = function(_, opts)
-    require("mini.surround").setup(opts)
-    require("neoverse.utils").on_load("which-key.nvim", function()
-      require("which-key").register({
-        mode = { "n", "x" },
-        ["s"] = { name = "surround" },
-      })
-    end)
-  end,
+  {
+    "echasnovski/mini.surround",
+    keys = function(_, keys)
+      -- Populate the keys based on the user's options
+      local plugin = require("lazy.core.config").spec.plugins["mini.surround"]
+      local opts = require("lazy.core.plugin").values(plugin, "opts", false)
+      local mappings = {
+        { opts.mappings.add, desc = "Surround » Open/add", mode = { "n", "v" } },
+        { opts.mappings.delete, desc = "Surround » Delete" },
+        { opts.mappings.find, desc = "Surround » Find next" },
+        { opts.mappings.find_left, desc = "Surround » Find prev" },
+        { opts.mappings.highlight, desc = "Surround » Highlight" },
+        { opts.mappings.replace, desc = "Surround » Replace/subt" },
+        { opts.mappings.update_n_lines, desc = "Surround » Update 'n' lines config" },
+      }
+      mappings = vim.tbl_filter(function(m)
+        return m[1] and #m[1] > 0
+      end, mappings)
+      return vim.list_extend(mappings, keys)
+    end,
+
+    opts = {
+      silent = true,
+      respect_selection_type = true,
+      mappings = {
+        add = "so",
+        delete = "sd",
+        replace = "sc",
+        find_left = "",
+        find = "",
+        highlight = "sh",
+        update_n_lines = "",
+      },
+    },
+  },
 }
