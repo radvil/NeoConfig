22b744ec064ae9b0e0fd3900864b3261a3d0e416
Radvil <radvil.linux@gmail.com>
2024-03-21 01:06:34 +0800

    fix(obsidian): properly register keymap prefix to whichkey

--- lua/neoverse/extras/editor/obsidian-nvim.lua
+++ lua/neoverse/extras/editor/obsidian-nvim.lua
@@ -16,91 +16,95 @@ local create_note_with_title = function()
   end)
 end
 
-local M = {
-  "epwalsh/obsidian.nvim",
-  dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
-  lazy = true,
+return {
+  {
+    "folke/which-key.nvim",
+    optional = true,
+    opts = {
+      defaults = {
+        ["<leader>n"] = { name = "note[obsidian]" },
+      },
+    },
+  },
+
+  {
+    "epwalsh/obsidian.nvim",
+    dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
+    lazy = true,
+    keys = {
+      {
+        "<leader>nc",
+        create_note_with_title,
+        desc = "note[obsidian] » create new note",
+      },
+      {
+        "<leader>no",
+        "<cmd>ObsidianOpen<cr>",
+        desc = "note[obsidian] » open",
+      },
+      {
+        "<leader>nn",
+        "<cmd>ObsidianToday<cr>",
+        desc = "note[obsidian] » today/now note",
+      },
+      {
+        "<leader>ny",
+        "<cmd>ObsidianYesterday<cr>",
+        desc = "note[obsidian] » yesterday note",
+      },
+      {
+        "<leader>nb",
+        ft = "markdown",
+        "<cmd>ObsidianBacklinks<cr>",
+        desc = "obsidian » show note's backlinks",
+      },
+      {
+        "<leader>nt",
+        ft = "markdown",
+        "<cmd>ObsidianTemplate<cr>",
+        desc = "obsidian » insert a template",
+      },
+      {
+        "gF",
+        ft = "markdown",
+        "<cmd>ObsidianLinkNew<cr>",
+        desc = "obsidian » link to new note",
+        mode = { "v", "x" },
+      },
+    },
+    opts = {
+      mappings = {},
+      finder = "telescope.nvim",
+      completion = { nvim_cmp = true },
+      daily_notes = {
+        folder = "+daily",
+        date_format = "%Y-%m-%d",
+      },
+      follow_url_func = function(url)
+        vim.fn.jobstart({ "xdg-open", url })
+      end,
+      note_id_func = function(title)
+        local suffix = ""
+        if title ~= nil then
+          suffix = title:gsub(" ", "-"):gsub("[^A-Za-z0-9-]", ""):lower()
+        else
+          for _ = 1, 4 do
+            suffix = suffix .. string.char(math.random(65, 90))
+          end
+        end
+        return tostring(os.time()) .. "-" .. suffix
+      end,
+    },
+    config = function(_, opts)
+      require("obsidian").setu(opts)
+      local Utils = require("neoverse.utils")
+      Utils.map("n", "gf", function()
+        if require("obsidian").util.cursor_on_markdown_link() then
+          return "<cmd>ObsidianFollowLink<cr>"
+        else
+          return "gf"
+        end
+      end, { desc = "obsidian » follow link" })
+    end,
+  },
 }
-
-M.keys = {
-  {
-    "<leader>nc",
-    create_note_with_title,
-    desc = "note[obsidian] » create new note",
-  },
-  {
-    "<leader>no",
-    "<cmd>ObsidianOpen<cr>",
-    desc = "note[obsidian] » open",
-  },
-  {
-    "<leader>nn",
-    "<cmd>ObsidianToday<cr>",
-    desc = "note[obsidian] » today/now note",
-  },
-  {
-    "<leader>ny",
-    "<cmd>ObsidianYesterday<cr>",
-    desc = "note[obsidian] » yesterday note",
-  },
-  {
-    "<leader>nb",
-    ft = "markdown",
-    "<cmd>ObsidianBacklinks<cr>",
-    desc = "obsidian » show note's backlinks",
-  },
-  {
-    "<leader>nt",
-    ft = "markdown",
-    "<cmd>ObsidianTemplate<cr>",
-    desc = "obsidian » insert a template",
-  },
-  {
-    "gF",
-    ft = "markdown",
-    "<cmd>ObsidianLinkNew<cr>",
-    desc = "obsidian » link to new note",
-    mode = { "v", "x" },
-  },
-}
-
-M.opts = {
-  mappings = {},
-  finder = "telescope.nvim",
-  completion = { nvim_cmp = true },
-  daily_notes = {
-    folder = "+daily",
-    date_format = "%Y-%m-%d",
-  },
-  follow_url_func = function(url)
-    vim.fn.jobstart({ "xdg-open", url })
-  end,
-  note_id_func = function(title)
-    local suffix = ""
-    if title ~= nil then
-      suffix = title:gsub(" ", "-"):gsub("[^A-Za-z0-9-]", ""):lower()
-    else
-      for _ = 1, 4 do
-        suffix = suffix .. string.char(math.random(65, 90))
-      end
-    end
-    return tostring(os.time()) .. "-" .. suffix
-  end,
-}
-
-M.config = function(_, opts)
-  require("obsidian").setup(opts)
-  local Utils = require("neoverse.utils")
-  Utils.map("n", "gf", function()
-    if require("obsidian").util.cursor_on_markdown_link() then
-      return "<cmd>ObsidianFollowLink<cr>"
-    else
-      return "gf"
-    end
-  end, { desc = "obsidian » follow link" })
-  Utils.on_load("which-key.nvim", function()
-    require("which-key").register({ mode = "n", ["<leader>n"] = { name = "note[obsidian]" } })
-  end)
-end
-
-return M
