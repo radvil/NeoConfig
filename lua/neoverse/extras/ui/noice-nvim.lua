454c73efe64ac6fcb6fe48aa4a2d7c4bbbd454f4
Radvil <radvil.linux@gmail.com>
2024-03-21 05:37:49 +0800

    feat(noice.nvim): Properly register keymap to which-key.nvim

--- lua/neoverse/extras/ui/noice-nvim.lua
+++ lua/neoverse/extras/ui/noice-nvim.lua
@@ -1,107 +1,97 @@
 return {
-  "folke/noice.nvim",
-  event = "VeryLazy",
-  dependencies = { "MunifTanjim/nui.nvim", "rcarriga/nvim-notify" },
-    -- stylua: ignore
-    keys = {
-      { "<c-d>", function() if not require("noice.lsp").scroll(4) then return "<c-d>" end end, expr = true, desc = "Noice » Scroll forward", mode = {"i", "n", "s"} },
-      { "<c-u>", function() if not require("noice.lsp").scroll(-4) then return "<c-u>" end end, expr = true, desc = "Noice » Scroll backward", mode = {"i", "n", "s"} },
-      ---@diagnostic disable-next-line: param-type-mismatch
-      { "<S-Enter>", function() require("noice").redirect(vim.fn.getcmdline()) end, mode = "c", desc = "Noice » Redirect cmdline" },
-      { "<leader>ll", function() require("noice").cmd("last") end, desc = "Logger » Noice last message" },
-      { "<leader>lh", function() require("noice").cmd("history") end, desc = "Logger » Noice message history" },
-      { "<leader>la", function() require("noice").cmd("all") end, desc = "Logger » All noice messages" },
-      { "<leader>ld", function() require("noice").cmd("dismiss") end, desc = "Logger » Dismiss noice messages" },
+  {
+    "which-key.nvim",
+    optional = true,
+    opts = {
+      defaults = {
+        ["<leader>l"] = { name = "logger/noice" },
+      },
     },
+  },
 
-  ---@param opts NoiceConfig
-  opts = function(_, opts)
-    local defaults = {
-      presets = {
-        inc_rename = true,
-        bottom_search = true,
-        lsp_doc_border = true,
+  {
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    dependencies = { "MunifTanjim/nui.nvim", "rcarriga/nvim-notify" },
+      -- stylua: ignore
+      keys = {
+        { "<c-d>", function() if not require("noice.lsp").scroll(4) then return "<c-d>" end end, expr = true, desc = "Noice » Scroll forward", mode = {"i", "n", "s"} },
+        { "<c-u>", function() if not require("noice.lsp").scroll(-4) then return "<c-u>" end end, expr = true, desc = "Noice » Scroll backward", mode = {"i", "n", "s"} },
+        ---@diagnostic disable-next-line: param-type-mismatch
+        { "<S-Enter>", function() require("noice").redirect(vim.fn.getcmdline()) end, mode = "c", desc = "Noice » Redirect cmdline" },
+        { "<leader>ll", function() require("noice").cmd("last") end, desc = "Logger » Noice last message" },
+        { "<leader>lh", function() require("noice").cmd("history") end, desc = "Logger » Noice message history" },
+        { "<leader>la", function() require("noice").cmd("all") end, desc = "Logger » All noice messages" },
+        { "<leader>ld", function() require("noice").cmd("dismiss") end, desc = "Logger » Dismiss noice messages" },
       },
-      messages = {
-        enabled = true,
-        view = "mini",
-        view_error = "mini",
-        view_warn = "notify",
-      },
-      lsp = {
-        progress = { enabled = false },
-        ---message shown by lsp servers
-        message = { enabled = true, view = "mini" },
-        hover = { enabled = true, silent = true },
-        override = {
-          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-          ["vim.lsp.util.stylize_markdown"] = true,
-          ["cmp.entry.get_documentation"] = true,
+
+    ---@param opts NoiceConfig
+    opts = function(_, opts)
+      local defaults = {
+        presets = {
+          inc_rename = true,
+          bottom_search = true,
+          lsp_doc_border = true,
         },
-        signature = {
+        messages = {
           enabled = true,
-          auto_open = {
+          view = "mini",
+          view_error = "mini",
+          view_warn = "notify",
+        },
+        lsp = {
+          progress = { enabled = false },
+          ---message shown by lsp servers
+          message = { enabled = true, view = "mini" },
+          hover = { enabled = true, silent = true },
+          override = {
+            ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+            ["vim.lsp.util.stylize_markdown"] = true,
+            ["cmp.entry.get_documentation"] = true,
+          },
+          signature = {
             enabled = true,
-            trigger = true,
-            luasnip = true,
-            throttle = 50,
+            auto_open = {
+              enabled = true,
+              trigger = true,
+              luasnip = true,
+              throttle = 50,
+            },
           },
         },
-      },
-      views = {
-        ---display cmdline and popup menu together
-        cmdline_popup = {
-          relative = "editor",
-          position = { col = "50%", row = 6 },
-          size = { width = 60, height = "auto" },
-          border = { style = "single", padding = { 0, 1 } },
+        views = {
+          ---display cmdline and popup menu together
+          cmdline_popup = {
+            relative = "editor",
+            position = { col = "50%", row = 6 },
+            size = { width = 60, height = "auto" },
+            border = { style = "single", padding = { 0, 1 } },
+          },
+          popupmenu = {
+            relative = "editor",
+            size = { width = 60, height = 10 },
+            position = { col = "50%", row = 10 },
+            border = { style = "single", padding = { 0, 1 } },
+          },
         },
-        popupmenu = {
-          relative = "editor",
-          size = { width = 60, height = 10 },
-          position = { col = "50%", row = 10 },
-          border = { style = "single", padding = { 0, 1 } },
+        routes = {
+          { view = "notify", filter = { event = "msg_showmode" } },
         },
-      },
-      routes = {
-        ---show @recording as notify message
-        { view = "notify", filter = { event = "msg_showmode" } },
-        -- {
-        --   view = "mini",
-        --   filter = {
-        --     event = "msg_show",
-        --     any = {
-        --       { find = "%d+L, %d+B" },
-        --       { find = "; after #%d+" },
-        --       { find = "; before #%d+" },
-        --     },
-        --   },
-        -- },
-      },
-    }
+      }
 
-    opts = vim.tbl_deep_extend("force", defaults, opts or {}) or defaults
+      opts = vim.tbl_deep_extend("force", defaults, opts or {}) or defaults
 
-    if opts and not vim.g.neo_transparent then
-      local win_opts = { winhighlight = "NormalFloat:NormalFloat,FloatBorder:FloatBorder" }
-      local border_opts = { style = "none", padding = { 1, 2 } }
-      opts.views.popupmenu.border = border_opts
-      opts.views.cmdline_popup.border = border_opts
-      opts.views.popupmenu.win_options = win_opts
-      opts.views.cmdline_popup.win_options = win_opts
-      opts.presets.lsp_doc_border = { views = { hover = { border = border_opts } } }
-    end
+      if opts and not vim.g.neo_transparent then
+        local win_opts = { winhighlight = "NormalFloat:NormalFloat,FloatBorder:FloatBorder" }
+        local border_opts = { style = "none", padding = { 1, 2 } }
+        opts.views.popupmenu.border = border_opts
+        opts.views.cmdline_popup.border = border_opts
+        opts.views.popupmenu.win_options = win_opts
+        opts.views.cmdline_popup.win_options = win_opts
+        opts.presets.lsp_doc_border = { views = { hover = { border = border_opts } } }
+      end
 
-    return opts
-  end,
-
-  config = function(_, opts)
-    require("noice").setup(opts)
-    require("neoverse.utils").on_load("which-key.nvim", function()
-      require("which-key").register({
-        mode = "n",
-        ["<leader>l"] = { name = "logger/noice" },
-      })
-    end)
-  end,
+      return opts
+    end,
+  },
 }
