2ff150940d7a655325b4dd3a6cf2d29657df9376
Radvil <radvil.linux@gmail.com>
2024-03-23 01:58:17 +0800

    feat(telescope-fzf-native): enable build with cmake
    Following upstream https://github.com/LazyVim/LazyVim/commit/1c13a5c10b745f741ebbb08d3963862e9d4e307e

--- lua/neoverse/core/plugins.lua
+++ lua/neoverse/core/plugins.lua
@@ -103,7 +103,9 @@ return {
     dependencies = {
       {
         "nvim-telescope/telescope-fzf-native.nvim",
-        build = "make",
+        build = vim.fn.executable("make") == 1 and "make"
+          or "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
+        enabled = vim.fn.executable("make") == 1 or vim.fn.executable("cmake") == 1,
         config = function()
           require("telescope").load_extension("fzf")
         end,
