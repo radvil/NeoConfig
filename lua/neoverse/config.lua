ddb66aed3041554c2ae3ab6a22c029f8cf81713a
Radvil <radvil.linux@gmail.com>
2024-04-04 05:17:36 +0800

    feat(config): follow upstream, add kind_filter

--- lua/neoverse/config.lua
+++ lua/neoverse/config.lua
@@ -94,6 +94,43 @@ local defaults = {
       Dots = "…",
     },
   },
+
+  ---@type table<string, string[]|boolean>?
+  kind_filter = {
+    markdown = false,
+    help = false,
+    default = {
+      "Class",
+      "Constructor",
+      "Enum",
+      "Field",
+      "Function",
+      "Interface",
+      "Method",
+      "Module",
+      "Namespace",
+      "Package",
+      "Property",
+      "Struct",
+      "Trait",
+    },
+    -- you can specify a different filter for each filetype
+    lua = {
+      "Class",
+      "Constructor",
+      "Enum",
+      "Field",
+      "Function",
+      "Interface",
+      "Method",
+      "Module",
+      "Namespace",
+      -- "Package", -- remove package since luals uses it for control flow structures
+      "Property",
+      "Struct",
+      "Trait",
+    },
+  },
 }
 
 M.json = {
@@ -122,6 +159,25 @@ end
 
 ---@type NeoVerseOpts | nil
 local options = nil
+
+---@param buf? number
+---@return string[]?
+function M.get_kind_filter(buf)
+  buf = (buf == nil or buf == 0) and vim.api.nvim_get_current_buf() or buf
+  local ft = vim.bo[buf].filetype
+  if M.kind_filter == false then
+    return
+  end
+  if M.kind_filter[ft] == false then
+    return
+  end
+  if type(M.kind_filter[ft]) == "table" then
+    ---@diagnostic disable-next-line: return-type-mismatch
+    return M.kind_filter[ft]
+  end
+  ---@diagnostic disable-next-line: return-type-mismatch
+  return type(M.kind_filter) == "table" and type(M.kind_filter.default) == "table" and M.kind_filter.default or nil
+end
 
 ---@param name "autocmds" | "options" | "keymaps"
 function M.load(name)
